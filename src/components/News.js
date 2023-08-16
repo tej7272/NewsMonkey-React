fa58bed95a494956f553565fb212e7523f355a52
tej7272 <tej7272@gmail.com>
2023-08-16 21:33:39 +0530

    news website

--- src/components/News.js
+++ src/components/News.js
@@ -16,7 +16,7 @@ const News =(props)=> {
   }
    const updateNews= async ()=>{
     props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=5dfaf24c15f546a7b263f1fa564edb92&page=${page}&pageSize=${props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
     setLoading(true);
     try{
       let data = await fetch(url);
@@ -42,7 +42,7 @@ const News =(props)=> {
   const fetchMoreData = async () => {
     setPage(page+1);
     
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=5dfaf24c15f546a7b263f1fa564edb92&page=${page+1}&pageSize=${props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     setArticles(articles.concat(parsedData.articles));
